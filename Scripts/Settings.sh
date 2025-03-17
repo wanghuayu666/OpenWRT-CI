07e5a9063c86a9d6f7c380ecea10acb8007728b2
wanghuayu666 <jerryrsjobs@gmail.com>
2025-03-17 11:03:57 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -27,27 +27,47 @@ if [ -n "$WRT_PACKAGE" ]; then
 fi
 
 # 配置网口
-NETWORK_CFG="./package/base-files/files/etc/config/network"
+CFG_FILE="./package/base-files/files/bin/config_generate"
 
 # 配置 eth0 和 eth3 绑定为 br-lan 网桥
-sed -i "/config interface 'lan'/a\    option ifname 'eth0 eth3'" $NETWORK_CFG
-sed -i "/config interface 'lan'/a\    option type 'bridge'" $NETWORK_CFG
+if ! grep -q "option ifname 'eth0 eth3'" $CFG_FILE; then
+    sed -i "/config interface 'lan'/a\    option ifname 'eth0 eth3'" $CFG_FILE
+fi
+if ! grep -q "option type 'bridge'" $CFG_FILE; then
+    sed -i "/config interface 'lan'/a\    option type 'bridge'" $CFG_FILE
+fi
 
 # 配置 eth1 为 WAN 口，拨号模式 (假设使用 PPPoE)
-sed -i "/config interface 'wan'/a\    option ifname 'eth1'" $NETWORK_CFG
-sed -i "/config interface 'wan'/a\    option proto 'pppoe'" $NETWORK_CFG
-sed -i "/config interface 'wan'/a\    option username '$WRT_WAN_USER'" $NETWORK_CFG
-sed -i "/config interface 'wan'/a\    option password '$WRT_WAN_PASSWORD'" $NETWORK_CFG
+if ! grep -q "option ifname 'eth1'" $CFG_FILE; then
+    sed -i "/config interface 'wan'/a\    option ifname 'eth1'" $CFG_FILE
+fi
+if ! grep -q "option proto 'pppoe'" $CFG_FILE; then
+    sed -i "/config interface 'wan'/a\    option proto 'pppoe'" $CFG_FILE
+fi
+if [ -n "$WRT_WAN_USER" ] && [ -n "$WRT_WAN_PASSWORD" ]; then
+    sed -i "/config interface 'wan'/a\    option username '$WRT_WAN_USER'" $CFG_FILE
+    sed -i "/config interface 'wan'/a\    option password '$WRT_WAN_PASSWORD'" $CFG_FILE
+fi
 
 # 配置 eth2 为 WAN1 口，拨号模式 (假设使用 PPPoE)
-sed -i "/config interface 'wan1'/a\    option ifname 'eth2'" $NETWORK_CFG
-sed -i "/config interface 'wan1'/a\    option proto 'pppoe'" $NETWORK_CFG
-sed -i "/config interface 'wan1'/a\    option username '$WRT_WAN1_USER'" $NETWORK_CFG
-sed -i "/config interface 'wan1'/a\    option password '$WRT_WAN1_PASSWORD'" $NETWORK_CFG
+if ! grep -q "option ifname 'eth2'" $CFG_FILE; then
+    sed -i "/config interface 'wan1'/a\    option ifname 'eth2'" $CFG_FILE
+fi
+if ! grep -q "option proto 'pppoe'" $CFG_FILE; then
+    sed -i "/config interface 'wan1'/a\    option proto 'pppoe'" $CFG_FILE
+fi
+if [ -n "$WRT_WAN1_USER" ] && [ -n "$WRT_WAN1_PASSWORD" ]; then
+    sed -i "/config interface 'wan1'/a\    option username '$WRT_WAN1_USER'" $CFG_FILE
+    sed -i "/config interface 'wan1'/a\    option password '$WRT_WAN1_PASSWORD'" $CFG_FILE
+fi
 
 # 配置 LAN 口 DHCP 从 192.168.88.10 开始
-sed -i "/config interface 'lan'/a\    option start '10'" $NETWORK_CFG
-sed -i "/config interface 'lan'/a\    option limit '100'" $NETWORK_CFG
-sed -i "/config interface 'lan'/a\    option leasetime '12h'" $NETWORK_CFG
-
-# 如果有其他网络配置修改，继续在此添加
+if ! grep -q "option start '10'" $CFG_FILE; then
+    sed -i "/config interface 'lan'/a\    option start '10'" $CFG_FILE
+fi
+if ! grep -q "option limit '100'" $CFG_FILE; then
+    sed -i "/config interface 'lan'/a\    option limit '100'" $CFG_FILE
+fi
+if ! grep -q "option leasetime '12h'" $CFG_FILE; then
+    sed -i "/config interface 'lan'/a\    option leasetime '12h'" $CFG_FILE
+fi
